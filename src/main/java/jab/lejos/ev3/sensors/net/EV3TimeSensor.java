d5a14fb56ea29f0e0e29bb2b958f30d8294cf841
jabrena <bren@juanantonio.info>
2013-09-30 23:26:20 +0200

    Updating EV3TimeSensor

--- src/main/java/jab/lejos/ev3/sensors/net/EV3TimeSensor.java
+++ src/main/java/jab/lejos/ev3/sensors/net/EV3TimeSensor.java
@@ -1,49 +1,34 @@
-package lejos.nxt.addon.net;
+package jab.lejos.ev3.sensors.net;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.security.URIParameter;
 
 import javax.microedition.location.Coordinates;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.xml.sax.SAXException;
 
-import com.sun.xml.internal.txw2.Document;
-
 /**
- * http://api.geonames.org/timezoneJSON?lat=40.479732600000000000&lng=-3.589829900000040700&username=jabrena
- * http://api.geonames.org/timezone?lat=40.479732600000000000&lng=-3.589829900000040700&username=jabrena
+ * http://api.geonames.org/timezone?lat=40.479732600000000000&lng=-3.589829900000040700&username=username
  * 
  * @author jabrena
  *
  */
-public class EV3TimeNetSensor {
+public class EV3TimeSensor {
 
 	private String username;
 	private Coordinates coordinate;
 	
-	public EV3TimeNetSensor(final Coordinates coordinate, final String username){
+	public EV3TimeSensor(final Coordinates coordinate, final String username){
 		this.coordinate = coordinate;
 		this.username = username;
 	}
@@ -68,9 +53,7 @@ public class EV3TimeNetSensor {
 	
 	public DateTime getTime() throws IOException, ParserConfigurationException, SAXException, XMLStreamException{
 		DateTime dateTime = new DateTime();
-		
-		//final String WS = "http://api.geonames.org/timezone?lat=40.479732600000000000&lng=-3.589829900000040700&username=jabrena";
-    	
+
         URL url = new URL(this.getWSURL());
         URLConnection conn = url.openConnection();
 
@@ -98,15 +81,5 @@ public class EV3TimeNetSensor {
 		
         return dateTime;
 	}
-	
-    public static void main(String[] args) throws Exception {
-    	
-    	Coordinates coordinate = new Coordinates(40.479732600000000000,-3.589829900000040700);
-    	
-    	EV3TimeNetSensor tns = new EV3TimeNetSensor(coordinate, "jabrena");
-
-    	System.out.println(tns.getTime().toDate().toString());
-
-    }
 
 }
---
+++ src/test/java/jab/lejos/ev3/sensors/net/EV3TimeSensorTest.java
@@ -0,0 +1,24 @@
+package jab.lejos.ev3.sensors.net;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import javax.microedition.location.Coordinates;
+
+public class EV3TimeSensorTest {
+
+    public static void main(String[] args) throws Exception {
+    	
+    	//Madrid
+    	Coordinates coordinate = new Coordinates(40.479732600000000000,-3.589829900000040700);
+
+    	Properties prop = new Properties();
+    	prop.load(new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/geonames.properties"));
+
+    	EV3TimeSensor tns = new EV3TimeSensor(coordinate, prop.getProperty("username"));
+
+    	System.out.println(tns.getTime().toDate().toString());
+
+    }
+
+}
